7935aab84d05fd473369b11113edd33ef213da2c
Zicong Chen <870667475@qq.com>
2023-04-10 11:42:57 +0800

    update

--- src/main/java/com/theOnlineShop/websocket/webSocketCommand/enums/constantCommand.java
+++ src/main/java/com/theOnlineShop/websocket/webSocketCommand/enums/constantCommand.java
@@ -1,5 +1,50 @@
 package com.theOnlineShop.websocket.webSocketCommand.enums;
 
-public class constantCommand {
+public enum constantCommand {
+
+    Update("00","update"),
+    Camera("01","camera");
+
+    private String order;
+    private String name;
+
+    constantCommand(String order, String name) {
+        this.order = order;
+        this.name = name;
+    }
+
+    public static String getOrder(String name){
+        for (constantCommand cc : constantCommand.values()) {
+            if(cc.getName().equals(name)){
+                return cc.order;
+            }
+        }
+        return null;
+    }
+
+    public static String getName(String order){
+        for (constantCommand cc : constantCommand.values()) {
+            if(cc.getOrder().equals(order)){
+                return cc.name;
+            }
+        }
+        return null;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
